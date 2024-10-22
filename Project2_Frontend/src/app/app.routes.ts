ed3a94428ecb29792810afada30730720cd6e686
Benjamin Kalnbach <bak7@illinois.edu>
2024-10-22 16:18:58 -0500

    BASIC Anonymous Page Set up

--- Project2_Frontend/src/app/app.routes.ts
+++ Project2_Frontend/src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { AnonymousComponent } from './anonymous/anonymous.component';
 
 export const routes: Routes = [
     {
         path:'',
         component:HomeComponent
+    },
+    {
+        path:'anonymous',
+        component:AnonymousComponent
     }
 ];
