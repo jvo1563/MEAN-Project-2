d9588904f7faee8c5ac82974c1e218e1ad40fd10
Benjamin Kalnbach <bak7@illinois.edu>
2024-10-22 15:23:31 -0500

    Commiting initial angular

---
+++ Project2_Frontend/src/app/anonymous/anonymous.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-anonymous',
+  standalone: true,
+  imports: [],
+  templateUrl: './anonymous.component.html',
+  styleUrl: './anonymous.component.css'
+})
+export class AnonymousComponent {
+
+}
