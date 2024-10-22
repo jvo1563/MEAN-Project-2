9a0a64ebacfa5fc16582726b785af243eb333eae
Benjamin Kalnbach <bak7@illinois.edu>
2024-10-22 17:00:51 -0500

    EOD commit

--- Project2_Frontend/src/app/app.routes.ts
+++ Project2_Frontend/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AnonymousComponent } from './anonymous/anonymous.component';
+import { UserLoginComponent } from './user-login/user-login.component';
 
 export const routes: Routes = [
     {
@@ -10,5 +11,9 @@ export const routes: Routes = [
     {
         path:'anonymous',
         component:AnonymousComponent
+    },
+    {
+        path:'login',
+        component:UserLoginComponent
     }
 ];
---
+++ Project2_Frontend/src/app/user-login/user-login.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-user-login',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './user-login.component.html',
+  styleUrl: './user-login.component.css'
+})
+export class UserLoginComponent {
+  login = {username:'', password:''};
+}
