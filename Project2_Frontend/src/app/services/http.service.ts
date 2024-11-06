9975f2fd6ef222807b773deaf78113cdfdaa1eec
Benjamin Kalnbach <bak7@illinois.edu>
2024-11-06 08:39:07 -0600

    Changed to talk to gw

--- Project2_Frontend/src/app/services/http.service.ts
+++ Project2_Frontend/src/app/services/http.service.ts
@@ -19,13 +19,13 @@ export class HttpService {
   aws_gw_true_url: string = 'https://zj6lfhgilj.execute-api.us-east-1.amazonaws.com';
 
 
-  anonymous_post_report:string = '/report';
+  anonymous_post_report:string = '/report-public';
 
-  anonymous_post_buis:string = '/businessentity';
+  anonymous_post_buis:string = '/businessentity-public';
 
-  anonymous_get_statuses:string = '/statuses';
+  anonymous_get_statuses:string = '/statuses-public';
 
-  anonymous_get_categories:string = '/categories';
+  anonymous_get_categories:string = '/categories-public';
 
   private_report_endpoint:string = '/report';
 
@@ -57,7 +57,7 @@ export class HttpService {
   }
 
   createReport(new_report:Report): Observable<HttpResponse<any>>{
-    let response =  this.httpClient.post<any>(this.aws_gw_url + this.private_report_endpoint, 
+    let response =  this.httpClient.post<any>(this.aws_gw_true_url + this.private_report_endpoint, 
       {
         title: new_report.title,
         description: new_report.description,
@@ -76,20 +76,20 @@ export class HttpService {
   getAllReports(): Observable<HttpResponse<any>>{
     // let newHeaders = new HttpHeaders();
     // newHeaders = newHeaders.set('Authorization', `Bearer ${token}`);
-    return this.httpClient.get<any>(this.aws_gw_url+this.private_report_endpoint, {observe: 'response'});
+    return this.httpClient.get<any>(this.aws_gw_true_url+this.private_report_endpoint, {observe: 'response'});
   }
 
   getReportById( report_id: number): Observable<HttpResponse<any>>{
-    return this.httpClient.get<any>(this.aws_gw_url+this.private_report_endpoint+`/${report_id}`, {observe: 'response'});
+    return this.httpClient.get<any>(this.aws_gw_true_url+this.private_report_endpoint+`/${report_id}`, {observe: 'response'});
   }
 
   getReportsByAssignedId(user_id: number): Observable<HttpResponse<any>>{
-    return this.httpClient.get<any>(this.aws_gw_url+this.private_report_endpoint+`/assigned_to/${user_id}`, {observe: 'response'});
+    return this.httpClient.get<any>(this.aws_gw_true_url+this.private_report_endpoint+`/assigned_to/${user_id}`, {observe: 'response'});
   }
 
   updateReport(report_id:number,diff_report:Report): Observable<HttpResponse<any>>{
     
-    return this.httpClient.put<any>(this.aws_gw_url + this.private_report_endpoint + `/${report_id}`, 
+    return this.httpClient.put<any>(this.aws_gw_true_url + this.private_report_endpoint + `/${report_id}`, 
       {
         title: diff_report.title,
         description: diff_report.description,
@@ -106,7 +106,7 @@ export class HttpService {
 
   updateUpdatedAtReport(report_id:number,new_date:Date): Observable<HttpResponse<any>>{
     
-    return this.httpClient.put<any>(this.aws_gw_url + this.private_report_endpoint + `/${report_id}`, 
+    return this.httpClient.put<any>(this.aws_gw_true_url + this.private_report_endpoint + `/${report_id}`, 
       {
         updated_at:new_date
       },
@@ -116,14 +116,14 @@ export class HttpService {
 
   deleteReport(report_id: number):Observable<void>{
     
-    return this.httpClient.delete<void>(this.aws_gw_url+this.private_report_endpoint+`/${report_id}`);
+    return this.httpClient.delete<void>(this.aws_gw_true_url+this.private_report_endpoint+`/${report_id}`);
   }
 
   
 
   createCategory(new_category: {category_name:string, description:string}): Observable<HttpResponse<{id:number, category_name:string, description:string}>>{
     
-    return this.httpClient.post<{id:number, category_name:string, description:string}>(this.aws_gw_url+this.private_category_endpoint, 
+    return this.httpClient.post<{id:number, category_name:string, description:string}>(this.aws_gw_true_url+this.private_category_endpoint, 
       {
         category_name: new_category.category_name,
         desciption: new_category.description
@@ -141,21 +141,21 @@ export class HttpService {
 
   getAllCategories(): Observable<HttpResponse<{id:number, category_name:string, description:string}[]>>{
     
-    return this.httpClient.get<{id:number, category_name:string, description:string}[]>(this.aws_gw_url+this.private_category_endpoint,
+    return this.httpClient.get<{id:number, category_name:string, description:string}[]>(this.aws_gw_true_url+this.private_category_endpoint,
       {observe:'response'}
     );
   }
 
   getCategoryById(cat_id:number): Observable<HttpResponse<{id:number, category_name:string, description:string}>>{
     
-    return this.httpClient.get<{id:number, category_name:string, description:string}>(this.aws_gw_url+this.private_category_endpoint + `/${cat_id}`,
+    return this.httpClient.get<{id:number, category_name:string, description:string}>(this.aws_gw_true_url+this.private_category_endpoint + `/${cat_id}`,
       {observe:'response'}
     );
   }
 
   updateCategory(cat_id:number, diff_category: {id:number, category_name:string, description:string}): Observable<HttpResponse<{id:number, category_name:string, description:string}>>{
     
-    return this.httpClient.put<{id:number, category_name:string, description:string}>(this.aws_gw_url+this.private_category_endpoint + `/${cat_id}`,
+    return this.httpClient.put<{id:number, category_name:string, description:string}>(this.aws_gw_true_url+this.private_category_endpoint + `/${cat_id}`,
       {
         id:diff_category.id,
         category_name:diff_category.category_name,
@@ -167,14 +167,14 @@ export class HttpService {
 
   deleteCategory(cat_id:number):Observable<void>{
     
-    return this.httpClient.delete<void>(this.aws_gw_url+this.private_category_endpoint+`/${cat_id}`);
+    return this.httpClient.delete<void>(this.aws_gw_true_url+this.private_category_endpoint+`/${cat_id}`);
   }
 
 
 
 
   createStatus(new_status: {status_name:string}): Observable<HttpResponse<{id:number, status_name:string}>>{
-    return this.httpClient.post<{id:number, status_name:string}>(this.aws_gw_url+this.private_status_endpoint, 
+    return this.httpClient.post<{id:number, status_name:string}>(this.aws_gw_true_url+this.private_status_endpoint, 
       {
         status_name: new_status.status_name
       }, 
@@ -189,21 +189,21 @@ export class HttpService {
   }
 
   getAllStatus(): Observable<HttpResponse<{id:number, status_name:string}[]>>{
-    return this.httpClient.get<{id:number, status_name:string}[]>(this.aws_gw_url+this.private_status_endpoint,
+    return this.httpClient.get<{id:number, status_name:string}[]>(this.aws_gw_true_url+this.private_status_endpoint,
       {observe:'response'}
     );
   }
 
   getStatusById(status_id:number): Observable<HttpResponse<{id:number, status_name:string}>>{
     
-    return this.httpClient.get<{id:number, status_name:string}>(this.aws_gw_url+this.private_status_endpoint + `/${status_id}`,
+    return this.httpClient.get<{id:number, status_name:string}>(this.aws_gw_true_url+this.private_status_endpoint + `/${status_id}`,
       {observe:'response'}
     );
   }
 
   updateStatus(status_id:number, diff_status: {id:number, status_name:string}): Observable<HttpResponse<{id:number, status_name:string}>>{
     
-    return this.httpClient.put<{id:number, status_name:string}>(this.aws_gw_url+this.private_status_endpoint + `/${status_id}`,
+    return this.httpClient.put<{id:number, status_name:string}>(this.aws_gw_true_url+this.private_status_endpoint + `/${status_id}`,
       {
         id:diff_status.id,
         status_name:diff_status.status_name
@@ -214,14 +214,14 @@ export class HttpService {
 
   deleteStatus(status_id:number):Observable<void>{
     
-    return this.httpClient.delete<void>(this.aws_gw_url+this.private_status_endpoint+`/${status_id}`);
+    return this.httpClient.delete<void>(this.aws_gw_true_url+this.private_status_endpoint+`/${status_id}`);
   }
 
 
 
   createUser(new_user: UserEntity): Observable<HttpResponse<UserEntity>>{
     
-    return this.httpClient.post<UserEntity>(this.aws_gw_url+this.private_user_endpoint, 
+    return this.httpClient.post<UserEntity>(this.aws_gw_true_url+this.private_user_endpoint, 
       {
         email:new_user.email,
         first_name:new_user.first_name,
@@ -236,21 +236,21 @@ export class HttpService {
 
   getAllUsers(): Observable<HttpResponse<any[]>>{
     
-    return this.httpClient.get<any[]>(this.aws_gw_url+this.private_user_endpoint,
+    return this.httpClient.get<any[]>(this.aws_gw_true_url+this.private_user_endpoint,
       {observe:'response'}
     );
   }
 
   getUserById(user_id:number): Observable<HttpResponse<any>>{
     
-    return this.httpClient.get<any>(this.aws_gw_url+this.private_user_endpoint + `/${user_id}`,
+    return this.httpClient.get<any>(this.aws_gw_true_url+this.private_user_endpoint + `/${user_id}`,
       {observe:'response'}
     );
   }
 
   updateUser(user_id:number, diff_user: UserEntity): Observable<HttpResponse<any>>{
     
-    return this.httpClient.put<any>(this.aws_gw_url+this.private_user_endpoint + `/${user_id}`,
+    return this.httpClient.put<any>(this.aws_gw_true_url+this.private_user_endpoint + `/${user_id}`,
       {
         id: diff_user.id,
         email:diff_user.email,
@@ -266,7 +266,7 @@ export class HttpService {
 
   deleteUser(user_id:number):Observable<void>{
     
-    return this.httpClient.delete<void>(this.aws_gw_url+this.private_user_endpoint+`/${user_id}`);
+    return this.httpClient.delete<void>(this.aws_gw_true_url+this.private_user_endpoint+`/${user_id}`);
   }
 
 
@@ -289,7 +289,7 @@ export class HttpService {
 
   createBuisness(new_buisness: BuisnessEntity): Observable<HttpResponse<any>>{
     
-    return this.httpClient.post<any>(this.aws_gw_url+this.private_buisness_endpoint, 
+    return this.httpClient.post<any>(this.aws_gw_true_url+this.private_buisness_endpoint, 
       {
         report_id:new_buisness.report_id,
         name:new_buisness.name,
@@ -305,21 +305,21 @@ export class HttpService {
 
   getAllBuisnesss(): Observable<HttpResponse<BuisnessEntity[]>>{
     
-    return this.httpClient.get<BuisnessEntity[]>(this.aws_gw_url+this.private_buisness_endpoint,
+    return this.httpClient.get<BuisnessEntity[]>(this.aws_gw_true_url+this.private_buisness_endpoint,
       {observe:'response'}
     );
   }
 
   getBuisnessById(buisness_id:number): Observable<HttpResponse<BuisnessEntity>>{
     
-    return this.httpClient.get<BuisnessEntity>(this.aws_gw_url+this.private_buisness_endpoint + `/${buisness_id}`,
+    return this.httpClient.get<BuisnessEntity>(this.aws_gw_true_url+this.private_buisness_endpoint + `/${buisness_id}`,
       {observe:'response'}
     );
   }
 
   updateBuisness(buisness_id:number, diff_buisness: BuisnessEntity): Observable<HttpResponse<any>>{
     
-    return this.httpClient.put<any>(this.aws_gw_url+this.private_buisness_endpoint + `/${buisness_id}`,
+    return this.httpClient.put<any>(this.aws_gw_true_url+this.private_buisness_endpoint + `/${buisness_id}`,
       {
         id: diff_buisness.id,
         report_id:diff_buisness.report_id,
@@ -336,14 +336,14 @@ export class HttpService {
 
   deleteBuisness(buisness_id:number):Observable<void>{
     
-    return this.httpClient.delete<void>(this.aws_gw_url+this.private_buisness_endpoint+`/${buisness_id}`);
+    return this.httpClient.delete<void>(this.aws_gw_true_url+this.private_buisness_endpoint+`/${buisness_id}`);
   }
 
 
 
   createAnnotation(new_annotation: Annotation): Observable<HttpResponse<Annotation>>{
     
-    return this.httpClient.post<Annotation>(this.aws_gw_url+this.private_annotation_endpoint, 
+    return this.httpClient.post<Annotation>(this.aws_gw_true_url+this.private_annotation_endpoint, 
       {
         report_id:new_annotation.report_id,
         title:new_annotation.title,
@@ -357,21 +357,21 @@ export class HttpService {
 
   getAllAnnotations(): Observable<HttpResponse<Annotation[]>>{
     
-    return this.httpClient.get<Annotation[]>(this.aws_gw_url+this.private_annotation_endpoint,
+    return this.httpClient.get<Annotation[]>(this.aws_gw_true_url+this.private_annotation_endpoint,
       {observe:'response'}
     );
   }
 
   getAnnotationById(annotation_id:number): Observable<HttpResponse<Annotation>>{
     
-    return this.httpClient.get<Annotation>(this.aws_gw_url+this.private_annotation_endpoint + `/${annotation_id}`,
+    return this.httpClient.get<Annotation>(this.aws_gw_true_url+this.private_annotation_endpoint + `/${annotation_id}`,
       {observe:'response'}
     );
   }
 
   updateAnnotation(annotation_id:number, diff_annotation: Annotation): Observable<HttpResponse<Annotation>>{
     console.log(diff_annotation);
-    return this.httpClient.put<Annotation>(this.aws_gw_url+this.private_annotation_endpoint + `/${annotation_id}`,
+    return this.httpClient.put<Annotation>(this.aws_gw_true_url+this.private_annotation_endpoint + `/${annotation_id}`,
       {
         id: diff_annotation.id,
         report_id:diff_annotation.report_id,
@@ -386,6 +386,6 @@ export class HttpService {
 
   deleteAnnotation(annotation_id:number):Observable<void>{
     
-    return this.httpClient.delete<void>(this.aws_gw_url+this.private_annotation_endpoint+`/${annotation_id}`);
+    return this.httpClient.delete<void>(this.aws_gw_true_url+this.private_annotation_endpoint+`/${annotation_id}`);
   }
 }
